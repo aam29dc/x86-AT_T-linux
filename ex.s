418528715a738989f8c750c199087e4d92e74dde
Michael Arrotta <73267302+aam29dc@users.noreply.github.com>
2023-11-25 15:51:28 -0500

    Update ex.s

--- ex.s
+++ ex.s
@@ -9,10 +9,34 @@
 _start:
 	pushl %ebp
 	movl %esp, %ebp
+	subl $4, %esp		# NODE *h = 0;
+	movl $0, -4(%ebp)
 	
+	movl %esp, %ebx		# ebx = &h;
 	
-
-
+	pushl $1
+	pushl %ebx
+	call list_init		# this calls allocate_init which must be called before any allocations
+	popl %ebx
+	addl $4, %esp
+	
+	pushl $2
+	pushl %ebx
+	call list_prepend
+	popl %ebx
+	addl $4, %esp		# address of newhead in eax
+	
+	pushl $3
+	pushl $1
+	pushl %ebx
+	call list_addNodeAtPos
+	popl %ebx
+	addl $4, %esp
+	
+	pushl (%ebx)
+	call list_print
+	addl $4, %esp
+ 
 	leave
 	movl $1, %eax
 	int $0x80
